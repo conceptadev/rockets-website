4614e9e908cf83989f285083fefff59ce8b5279f
MrMaz <MrMaz@users.noreply.github.com>
2024-06-26 22:37:11 +0000

    deploy: bf9cd82ef0d1330e3b41964bfa42f94de641b794

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
-  },
-  ignorePatterns: ['**'],
-};
