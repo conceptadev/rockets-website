23b6b85e9a3fb859c97ddd75f872a176e7cbc31c
Marshall Sorenson <marshall.sorenson@gmail.com>
2024-07-16 20:54:18 -0400

    chore: explicit build required for yarn 4

--- build/docs-core.sh
+++ build/docs-core.sh
@@ -5,8 +5,11 @@ REPO=./.tmp/repos/rockets
 # clone
 git clone --depth=1 https://github.com/conceptadev/rockets.git $REPO
 
-# install and build
-yarn --cwd $REPO
+# install
+yarn --cwd $REPO --immutable
+
+# build
+yarn --cwd $REPO build
 
 # typedoc
 typedoc --options typedoc.rockets.json
--- build/docs-react.sh
+++ build/docs-react.sh
@@ -5,8 +5,11 @@ REPO=./.tmp/repos/rockets-react
 # clone
 git clone --depth=1 https://github.com/conceptadev/rockets-react.git $REPO
 
-# install and build
-yarn --cwd $REPO
+# install
+yarn --cwd $REPO --immutable
+
+# build
+yarn --cwd $REPO build
 
 # typedoc
 typedoc --options typedoc.rockets-react.json
